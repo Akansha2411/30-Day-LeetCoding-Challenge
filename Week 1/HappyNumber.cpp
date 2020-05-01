5bf79baa1dc46787a350bec9a682064203550fdf
Akansha Rai <62787261+Akansha2411@users.noreply.github.com>
2020-05-01 11:56:49 +0530

    Update HappyNumber.cpp

--- Week 1/HappyNumber.cpp
+++ Week 1/HappyNumber.cpp
@@ -24,33 +24,62 @@ Approach:
 #include <vector>
 #include <unordered_map>
 #include <math.h>
-using namespace std;
+using namespace std;  //O(1) solution
 
-class Solution {
-public:
-    bool isHappy(int n) {
-        unordered_map <int, int> checkRepeat;
-        int sum = 0;
-        while(1){
-            while(n>0){
-                sum+= pow(n%10,2);
-                n=n/10;
-            }
-            if(sum==1)
-                return true;
-            if(checkRepeat.count(sum)>0)
-                return false;
-            checkRepeat[sum] = 1;
-            n = sum;
-            sum = 0;
-            
-        }
+int digitSquareSum(int n) {
+    int sum = 0, tmp;
+    while (n) {
+        tmp = n % 10;
+        sum += tmp * tmp;
+        n /= 10;
     }
-};
-
-int main()
-{
-    Solution obj;
-    cout<<obj.isHappy(18);
-    return 0;
+    return sum;
 }
+ 
+bool isHappy(int n) {
+    int slow, fast;
+    slow = fast = n;
+    do {
+        slow = digitSquareSum(slow);
+        fast = digitSquareSum(fast);
+        fast = digitSquareSum(fast);
+    } while(slow != fast);
+    if (slow == 1) return 1;
+    else return 0;
+}
+
+
+/* Questions: Why unhappy number must end with a loop?
+There is a proof from wiki:
+https://en.wikipedia.org/wiki/Happy_number
+Numbers that are happy, follow a sequence that ends in 1. All non-happy numbers follow sequences that reach the cycle:
+4, 16, 37, 58, 89, 145, 42, 20, 4, ... */
+ 
+// method return true if n is Happy Number 
+// numSquareSum method is given in below detailed code snippet 
+
+int numSquareSum(int n) 
+{ 
+    int squareSum = 0; 
+    while (n) 
+    { 
+        squareSum += (n % 10) * (n % 10); 
+        n /= 10; 
+    } 
+    return squareSum; 
+}
+int isHappyNumber(int n)  //O(n) space complexity.
+{ 
+	set<int> st; 
+    
+	while (1) 
+	{ 
+		n = numSquareSum(n); 
+		if (n == 1) 
+			return true; 
+		if (st.find(n) != st.end()) 
+			return false; 
+		st.insert(n); 
+	} 
+} 
+ */
